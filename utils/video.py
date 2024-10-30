f812de32dfc06fa66af67695f146367a6ff95ac7
sreekanth-qb <sreekanths@qburst.com>
2024-10-30 22:13:49 +0530

    Update: prediction limited to every 7 frames for faster framerate

--- utils/video.py
+++ utils/video.py
@@ -1,5 +1,7 @@
 import cv2
 from time import time
+from yolo.findall import findall
+from find_bot import getnearestball
 
 
 def resize_to_max_dimensions(current_width, current_height, max_width, max_height, frame):
@@ -18,16 +20,23 @@ def resize_to_max_dimensions(current_width, current_height, max_width, max_heigh
     return cv2.resize(frame, (new_width, new_height))
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("http://192.168.41.116:4747/video")
     original_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     original_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     # Check if the video opened successfully
     if not cap.isOpened():
         print("Error: Could not open video.")
         exit()
+    frame_count = 0
+    skip_frame = 7
+    ball_value = []
+    ppcm = 50
+    radius = int(1 * ppcm)
+    balls = []
 
     while True:
-        success, frame = cap.read()  
+        
+        success, frame = cap.read()
         # Break the loop if there are no frames left
         if not success:
             print("End of video.")
@@ -35,6 +44,13 @@ if __name__ == "__main__":
     
         resized_frame = resize_to_max_dimensions(original_width, original_height,1500,800,frame=frame)
 
+        if frame_count % skip_frame == 0:
+            balls, _, _ = findall(resized_frame)
+        
+        if balls != []:
+            cv2.circle(resized_frame,balls[0], radius, (255,0,255), 4)
+
+        frame_count += 1
         cv2.imshow('Video', resized_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
