32379bd94d9d383b48762a49dfcb9607535f371e
Musammil <musammil@qburst.com>
2024-11-05 11:38:50 +0530

    v2 initialized

---
+++ bot_logic_v2/algorithm.py
@@ -0,0 +1,84 @@
+import time
+import cv2
+
+
+from const import BLUE, GREEN, RED
+from targetDetection import filter_balls, choose_next_target_point
+from util import draw_rectangle
+
+
+target_point,selected_point = None, None
+def select_point(event, x, y, flags, param):
+    global selected_point
+    if event == cv2.EVENT_LBUTTONDOWN:
+        selected_point = (x, y)
+        
+
+def algorithm(detection, bot):
+    global target_point,selected_point
+
+    print("Algorithm started!")
+    cv2.namedWindow("Feed" )
+    cv2.setMouseCallback("Feed",select_point)  
+    balls = None
+    while True:
+
+        frame = detection.video_stream.read() # remove
+
+        if balls is not None:
+            for ball in balls:
+                cv2.circle(frame, ball, 5, RED, -1)
+        if selected_point is not None:
+            detection_object = detection.process_frame()
+            bot_angle, bot_center_point, goal_center_point = detection_object['aruco']['bot_angle'], \
+                detection_object['aruco']['bot_center_point'], detection_object['aruco']['goal_center_point']
+
+            bot.updatePosition(bot_center_point, bot_angle)
+            time.sleep(0.1)
+            time_delay = bot.move(selected_point)
+            time.sleep((time_delay/1000)+0.5)
+            bot.makeMovement('right', 2000)
+            selected_point =None
+
+        draw_rectangle(frame, detection.trimmed_field,BLUE)
+        draw_rectangle(frame, detection.field_corners,GREEN)
+
+        cv2.imshow('Feed', frame)
+
+        pressed_key = cv2.waitKey(1)
+        if pressed_key == ord('q'):
+            print("Exiting...")
+            break
+        # elif pressed_key == ord('t'):
+        detection_object = detection.process_frame()
+        trimmed_field = detection.trimmed_field
+        bot_center_point, balls, goal_center_point =  detection_object['aruco']['bot_center_point'],detection_object['yolo']['balls'],detection_object['aruco']['goal_center_point']
+        if any([balls == [] , goal_center_point is None , bot_center_point is None]):
+            print(f"Detection failed goal post: {goal_center_point}, bot center point: {bot_center_point} no of balls:{len(balls)}")
+            continue
+        print(f"Detection failed goal post: {goal_center_point}, bot center point: {bot_center_point} no of balls:{len(balls)}")
+
+        filtered_balls, intersection_points = filter_balls(trimmed_field,balls, goal_center_point ,buffer_distance=20)
+        next_target_point = choose_next_target_point(intersection_points, bot_center_point)
+        if next_target_point is not None:
+            print('target locked')
+            target_point = next_target_point['target_point']
+            goal_point = next_target_point['goal_point']
+            frame = detection.video_stream.read()
+            cv2.circle(frame, target_point, 5, BLUE, -1)
+            cv2.circle(frame, goal_point, 5, GREEN, -1)
+            cv2.imshow('Feed', frame)
+            # elif pressed_key == ord('g'):
+            bot_center_point , bot_angle = detection.detection_object['aruco']['bot_center_point'], detection.detection_object['aruco']['bot_angle']
+            bot.updatePosition(bot_center_point, bot_angle)
+            time_delay = bot.move(target_point)
+            time.sleep((time_delay/1000)+0.5)
+            bot_angle, bot_center_point, goal_center_point, _ = detection.detect_aruco()
+            bot.updatePosition(bot_center_point, bot_angle)
+            time_delay = bot.move(goal_point)
+            time.sleep((time_delay/1000)+0.5)
+            print("Goal reached!")
+            target_point, goal_point = None, None
+            time.sleep(2)
+        else:
+            print("No extended points found within the trimmed field.")
---
+++ bot_logic_v2/main.py
@@ -0,0 +1,61 @@
+import time
+import cv2
+
+from detection import Detection
+from bot import Bot
+from algorithm import algorithm
+from util import draw_rectangle
+from const import BLUE, GREEN
+
+
+def main():
+    detection = Detection()
+    # Wait for initial bot detection
+    print("Waiting for initial bot detection...")
+    while True:
+        bot_angle, bot_center_point, _, _ = detection.detect_aruco()
+        if bot_angle is not None:
+            print(f"Bot angle:{bot_angle}, bot center point: {bot_center_point}")
+            break
+        time.sleep(0.5)
+    bot = Bot(bot_center_point, bot_angle,detection)
+
+    print("Initializing completed!")
+
+    # Wait for start key
+    try:
+        # Start Algorithm
+        algorithm(detection, bot)
+        # while True:
+        #     image = detection.video_stream.read()
+
+        #     cv2.imshow('Feed', image)
+        #     pressed_key = cv2.waitKey(1)
+        #     if pressed_key == ord('q'):
+        #         print("Exiting...")
+        #         break
+        #     elif pressed_key == ord('i'):
+        #         bot_angle, bot_center_point, _, _ = detection.detect_aruco()
+        #         print("initial point")
+        #         print(f"Bot angle:{bot_angle}, bot center point: {bot_center_point}")
+        #         movement = input("movement: ")
+        #         interval = input("interval: ")
+        #         print("final point")
+        #         bot.makeMovement(movement, interval)
+        #         time.sleep(1)
+        #         bot_angle_2, bot_center_point_2, _, _ = detection.detect_aruco()
+        #         print(f"Bot angle:{bot_angle_2}, bot center point: {bot_center_point_2}")
+        #     elif pressed_key == ord('d'):
+        #         bot_angle_2, bot_center_point_2, _, _ = detection.detect_aruco()
+        #         # print("initial point")
+        #         print(f"Bot angle:{bot_angle_2}, bot center point: {bot_center_point_2}")
+
+
+        #     time.sleep(0.01)
+
+    except KeyboardInterrupt:
+        detection.destroy()
+        print("Exiting...")
+
+if __name__ == "__main__":
+    main()
