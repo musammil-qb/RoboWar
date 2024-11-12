a3f4f11bab15e0b2a427df321c7f57b859e8c7da
Musammil <musammil@qburst.com>
2024-11-12 18:00:39 +0530

    init v4

---
+++ bot_logic_v4/util.py
@@ -0,0 +1,19 @@
+import math
+import cv2
+import numpy as np
+from const import GREEN
+
+# Function to calculate the distance between two points
+def calculate_distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
+# Function to calculate the angle between two points
+def calculate_angle_to_point(bot_position, target_position):
+    dx = target_position[0] - bot_position[0]
+    dy = target_position[1] - bot_position[1]
+    angle_to_target = math.degrees(math.atan2(dy, dx)) % 360
+    return angle_to_target
+
+def draw_rectangle(frame, corners,color=GREEN):
+    if len(corners) == 4:
+        cv2.polylines(frame, [np.array(corners)], isClosed=True, color=color, thickness=2)
